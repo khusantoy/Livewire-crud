e0b8f792b537b977796e685aa21440200b5f53d7
Khusanboy <119614237+khusanboytursunov@users.noreply.github.com>
2023-02-14 15:13:36 +0500

    created Userslivewire

---
+++ app/Http/Livewire/UsersLivewire.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class UsersLivewire extends Component
+{
+    public function render()
+    {
+        return view('livewire.users-livewire');
+    }
+}
